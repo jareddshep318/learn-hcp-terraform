ad7649b44c5bcf6f2c06b2f83bd5529e7b355244
jareddshep318 <jareddshep318@gmail.com>
2026-05-12 02:13:46 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,11 +10,4 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type      = string
-  sensitive = true
-}
